7fc78a75d17df1988152e737a2cadb2e8b172bd4
Adam <adamcpenman@gmail.com>
2020-01-14 20:19:34 -0600

    code for guided projects

--- users/users-model.js
+++ users/users-model.js
@@ -1,4 +1,5 @@
 const db = require("../database/dbConfig")
+const bcrypt = require("bcryptjs")
 
 function find() {
   return db("users")
@@ -12,6 +13,8 @@ function findBy(filter) {
 }
 
 async function add(user) {
+  user.password = await bcrypt.hash(user.password, 14)
+
   const [id] = await db("users")
     .insert(user)
  
